f57d5c2fa21dc2839e98457424ffa1bc064df562
Aezrael1 <23110040@iitgn.ac.in>
2024-08-16 16:50:51 +0530

    Tree functions

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
 from typing import Union
 import pandas as pd
+import math
 
 
 def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
@@ -13,7 +14,7 @@ def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     ensure that the function does not fail in corner cases.
     """
     assert y_hat.size == y.size
-    # TODO: Write here
+    return ((y_hat == y).sum())/len(y)
     pass
 
 
@@ -21,6 +22,12 @@ def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the precision
     """
+
+    assert y_hat.size == y.size
+    
+    z = (y == cls)
+    z_hat = (y_hat == cls)
+    return (((z_hat == True) & (z == True)).sum())/(z_hat.sum())
     pass
 
 
@@ -28,6 +35,10 @@ def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the recall
     """
+    assert y_hat.size == y.size
+    z = (y == cls)
+    z_hat = (y_hat == cls)
+    return (((z_hat == True) & (z == True)).sum())/(z.sum())
     pass
 
 
@@ -35,7 +46,8 @@ def rmse(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the root-mean-squared-error(rmse)
     """
-
+    z = math.sqrt((abs(y_hat - y)**2).sum()/len(y))
+    return z
     pass
 
 
@@ -43,4 +55,7 @@ def mae(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the mean-absolute-error(mae)
     """
+    z = (abs(y_hat - y)).sum()/len(y)
+    return z
     pass
+
