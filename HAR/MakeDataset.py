d0e5d20432726edf6ad14e04d4c0e42d7e6187f3
Abhinav-Khot <23110006@iitgn.ac.in>
2024-08-17 19:53:09 +0530

    task 2-3 in prog

--- HAR/MakeDataset.py
+++ HAR/MakeDataset.py
@@ -81,5 +81,3 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=
 print("Training data shape: ",X_train.shape)
 print("Testing data shape: ",X_test.shape)
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-
